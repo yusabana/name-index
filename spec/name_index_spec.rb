fcc6aeb54f42720ed92d680105d44b0a80ba29c2
Junichi Ito <jit@sonicgarden.jp>
2014-05-21 04:30:19 +0900

    Add spec.

--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -3,4 +3,5 @@ require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 describe NameIndex do
   specify { expect(NameIndex.create_index([])).to eq [] }
   specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']] ] }
+  specify { expect(NameIndex.create_index(['オカダ', 'ホリタ', 'ポリタ', 'ボリタ', 'ヨコヤマ'])).to eq [ ['ア', ['オカダ']], ['ハ', ['ホリタ', 'ボリタ', 'ポリタ']], ['ヤ', ['ヨコヤマ']] ] }
 end
