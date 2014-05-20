e1b43eb794acf4d033561b9c2d659819fd46af54
Junichi Ito <jit@sonicgarden.jp>
2014-05-21 04:26:58 +0900

    Answered.

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,13 @@
 class NameIndex
-  def self.create_index(names)
-    []
+  GROUP_CHARS = %w(ア カ サ タ ナ ハ マ ヤ ラ ワ ン).freeze
+
+  class << self
+    def create_index(names)
+      names.sort.group_by{|name| group_char_for(name) }.to_a
+    end
+
+    def group_char_for(name)
+      GROUP_CHARS.each_cons(2).find {|from, to| (from...to).include? name[0] }.first
+    end
   end
 end
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index('')).to eq [] }
+  specify { expect(NameIndex.create_index([])).to eq [] }
+  specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ'])).to eq [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']] ] }
 end
