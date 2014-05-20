00908d17098eed2ffe88d6fce955e435191c291e
Junichi Ito <jit@sonicgarden.jp>
2014-05-21 04:31:16 +0900

    Fix sample.

--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index('')).to eq [] }
+  specify { expect(NameIndex.create_index([])).to eq [] }
 end
