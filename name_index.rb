e2cf461953d3e280ba3b06770e263a80993bba8a
yusabana <taka.esu.user@gmail.com>
2014-05-24 16:22:48 +0900

    imprements normal function exclude asonant mark

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,68 @@
 class NameIndex
   def self.create_index(names)
-    []
+    name_values = names.map {|name| NameValue.new(name)}
+    transform(name_values).to_a.sort
   end
+
+  # Dictionaryのコンテンツにマッチしたら出す
+  def self.transform(name_values)
+    ret = {}
+
+    name_values.each do |name_value|
+      key = Dictionary.get_key(name_value)
+      ret[key] ||= []
+      ret[key] << name_value.name
+    end
+
+    ret
+  end
+
+end
+
+class NameValue
+  attr_accessor :name, :index
+
+  # 濁点付きのの文字列だったら濁点なくした奴に変換する
+  def initialize(name)
+    @name = name
+    @index = name[0]
+  end
+end
+
+
+# あ行、か行 などのメタを使って動的生成させるといいかも
+class Dictionary
+  @@contents =
+    {
+      "ア" => Set["ア", "イ", "ウ", "エ", "オ"],
+      "カ" => Set["カ", "キ", "ク", "ケ", "コ"],
+      "サ" => Set["サ", "シ", "ス", "セ", "ソ"],
+      "タ" => Set["タ", "チ", "ツ", "テ", "ト"],
+      "ナ" => Set["ナ", "ニ", "ヌ", "ネ", "ノ"],
+      "ハ" => Set["ハ", "ヒ", "フ", "ヘ", "ホ"],
+      "マ" => Set["マ", "ミ", "ム", "メ", "モ"],
+      "ヤ" => Set["ヤ", "ユ", "ヨ"],
+      "ラ" => Set["ラ", "リ", "ル", "レ", "ロ"],
+      "ワ" => Set["ワ", "ヲ", "ン"],
+    }
+
+  def self.contents
+    @@contents
+  end
+
+  def self.get_key(name_value)
+    @@contents.each do |c_key, c_sets|
+      if c_sets.include?(name_value.index)
+        return c_key
+      end
+    end
+
+    raise "不正な文字 #{name_value.name}"
+  end
+end
+
+class DictionaryKey
+end
+
+class DictionaryContent
 end
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,29 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
-  specify { expect(NameIndex.create_index([])).to eq [] }
+  specify {
+    expect(NameIndex.create_index(['キシモト', 'イトウ']))
+      .to eq [ ['ア', ['イトウ']], ['カ', ['キシモト']] ]
+  }
+
+  # specify {
+  #   expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ']))
+  #     .to eq[ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']] ]
+  # }
+end
+
+
+
+describe NameValue do
+
+  describe "濁点付き" do
+    skip
+  end
+end
+
+
+
+describe Dictionary do
+
+  describe ""
 end
