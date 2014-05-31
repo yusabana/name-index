cc582c95abec21defefa05ea7b8ca8f888fac8f9
Yuji Takaesu <taka.esu.user@gmail.com>
2014-05-31 22:48:08 +0900

    refactoring variables and methods

--- name_index.rb
+++ name_index.rb
@@ -1,52 +1,45 @@
 class NameIndex
   def self.create_index(names)
-    name_values = names.map {|name| NameValue.new(name)}
-    transform(name_values)
+    transform(names.map{|name| NameValue.new(name)})
   end
 
   def self.transform(name_values)
-    name_values.each_with_object({}) do |name_value, result|
+    sorted_name_values = name_values.sort_by{|name_value| name_value.first}
+    sorted_name_values.each_with_object(Hash.new {|h, k| h[k] = []}) do |name_value, result|
       key = Dictionary.get_key(name_value)
-      result[key] ||= []
       result[key] << name_value.name
-      result[key].sort!
-    end.sort.to_a
+    end.to_a
   end
 end
 
 class NameValue
-  attr_reader :name, :index
+  attr_reader :name, :first
 
   def initialize(name)
     @name = name
-    @index = name[0]
+    @first = name.chr
   end
 end
 
 # unicodeの並び順 http://pentan.info/doc/unicode_list.html
 class Dictionary
-  @@contents =
-    {
-      "ア" => "ア".."オ",
-      "カ" => "カ".."コ",
-      "サ" => "サ".."ソ",
-      "タ" => "タ".."ト",
-      "ナ" => "ナ".."ノ",
-      "ハ" => "ハ".."ホ",
-      "マ" => "マ".."モ",
-      "ヤ" => "ヤ".."ヨ",
-      "ラ" => "ラ".."ロ",
-      "ワ" => "ワ".."ン",
-    }
+  CONTENTS = {
+    "ア" => "ア"..."カ",
+    "カ" => "カ"..."サ",
+    "サ" => "サ"..."タ",
+    "タ" => "タ"..."ナ",
+    "ナ" => "ナ"..."ハ",
+    "ハ" => "ハ"..."マ",
+    "マ" => "マ"..."ヤ",
+    "ヤ" => "ヤ"..."ラ",
+    "ラ" => "ラ"..."ワ",
+    "ワ" => "ワ".."ン",
+  }.freeze
 
   def self.get_key(name_value)
-    @@contents.each do |c_key, c_range|
-      if c_range.include?(name_value.index)
-        return c_key
-      end
-    end
-
-    raise "invalid letter, #{name_value.name}"
+    CONTENTS.find do |c_index, c_range|
+      c_range.include?(name_value.first)
+    end.first
   end
 end
 
